c3ad207179dbb794fed88b582f305db686522934
chloegeller <43284594+chloegeller@users.noreply.github.com>
2022-05-12 12:28:22 -0400

    adding jspsych study1.0

---
+++ jspsych/src/experiments/study1.0/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Stimulus from "./stimulus";
+import Config from "./config.yaml";
+import Instructions from "./instructions";
+
+export default {
+    Config, Stimulus, Instructions,
+}
